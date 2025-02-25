55436c0b4f3a7a28f846b4d33560ee7b72586051
VICTOR HUGO GAETANO OLIVEIRA TAVARES DE PINHO <24.00947-4@maua.br>
2025-02-25 08:51:28 -0300

    atualização modalLogin e modalCadastro

---
+++ js/components/modalCadastro.js
@@ -0,0 +1,18 @@
+fetch('../../html/components/modalCadastro.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('my-modalCadastro-importacao').innerHTML = data;
+        
+        // Função para o cadastro (mostrar/ocultar senha)
+        const toggleRegisterPassword = document.querySelector('#toggleRegisterPassword');
+        const passwordCadastroInput = document.querySelector('#passwordCadastroInput');
+        const registerEyeIcon = document.querySelector('#registerEyeIcon');
+
+        toggleRegisterPassword.addEventListener('click', function () {
+            const type = passwordCadastroInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordCadastroInput.setAttribute('type', type);
+            registerEyeIcon.classList.toggle('fa-eye');
+            registerEyeIcon.classList.toggle('fa-eye-slash');
+        });
+    })
+    .catch(error => console.error('Erro ao carregar a página:', error));
