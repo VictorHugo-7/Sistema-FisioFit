7010e0573d8f4b74d78a0f82c5f109e50ba2530d
VictorHugo-7 <victorhotpinho7@gmail.com>
2025-02-27 19:27:57 -0300

    atualização: rodapé

--- src/js/components/rodape.js
+++ src/js/components/rodape.js
@@ -2,5 +2,8 @@ fetch('../../src/html/components/rodape.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('my-rodape-importacao').innerHTML = data;
+
+        // Script Rodapé
+        
     })
     .catch(error => console.error('Erro ao carregar a página:', error));
